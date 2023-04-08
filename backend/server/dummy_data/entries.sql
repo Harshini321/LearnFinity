6ea1fb6ae375f722a90f579f27b3eb439c9c2f35
ashish <ashisharora.dev@gmail.com>
2023-04-09 03:13:38 +0530

    dummy data sql

---
+++ backend/server/dummy_data/entries.sql
@@ -0,0 +1,14 @@
+INSERT INTO entry (entry_day, entry_start_time, entry_end_time, entry_insti_id)
+VALUES ('Tuesday', '08:00', '09:00', 2),
+       ('Wednesday', '08:00', '09:00', 2),
+       ('Friday', '08:00', '09:00', 2),
+       ('Tuesday', '09:00', '10:00', 2),
+       ('Wednesday', '09:00', '10:00', 2),
+       ('Friday', '09:00', '10:00', 2),
+       ('Tuesday', '10:00', '11:00', 2),
+       ('Thursday', '10:00', '11:00', 2),
+       ('Friday', '10:00', '11:00', 2),
+       ('Monday', '09:30', '11:00', 2),
+       ('Thursday', '09:30', '11:00', 2),
+       ('Monday', '17:00', '18:30', 2),
+       ('Thursday', '17:00', '18:30', 2);
